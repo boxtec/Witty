445af3bf9c553f9dbd2dcc2000926a64a8fd185e
Jean-Daniel Nicoud <nicoud@didel.com>
2019-05-12 19:56:27 +0200

    Update 'examples/Stencil/Stencil.ino'
    Better comments

--- examples/Stencil/Stencil.ino
+++ examples/Stencil/Stencil.ino
@@ -1,25 +1,23 @@
 // Stencil.ino  -- rename first and update this title
 // Empty file you can use if the WittyLib.zip has been loaded with Arduino libs
 // Cut and paste what you use:
-//  #include "Witty.h"    SetupWitty();
-//  #include "GetPush.h"  need Witty.h
-//  #include "Telec.h"    need Witty.h
-//  #include "Apa102.h"   SetupApa102(); 
-//  #include "Inter2.h"   SetupInter2(); application dependant
-//  #include "Pfm.h"      SetupPfm();
-//  #include "TerSer.h"   SetupTerSer();
-//  #include "I2Ctwi.h"   SetupI2Ctwi();
-//  SetupGy521();         need I2Ctwi.h
+//  #include "GetPush.h"  //need Witty.h
+//  #include "Telec.h"    //need Witty.h
+//  #include "Apa102.h"   //need SetupApa102(); 
+//  #include "Inter2.h"   //need SetupInter2(); application dependant
+//  #include "Pfm.h"      //need //need //need //need SetupPfm();
+//  #include "TerSer.h"   //need //need //need SetupTerSer();
+//  #include "I2Ctwi.h"   //need //need SetupI2Ctwi();
+//  #include "Gy521.h"    //need SetupGy521(); and I2Ctwi.h
 
 #include "Witty.h" 
 // global variables and functions
 
 void setup() {
   SetupWitty();
-
+  // add required Setups
 }
 
 void loop() {
-
 
 }
